774d76064405eeb9ef78b1a92a608176caefcd3d
影凌 <k6.developer@gmail.com>
2024-07-18 15:59:00 +0800

    modify: remove USE_L10N

--- RyoURL/RyoURL/settings.py
+++ RyoURL/RyoURL/settings.py
@@ -111,15 +111,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
 LANGUAGE_CODE = 'zh-Hant'
-
 TIME_ZONE = 'Asia/Taipei'
-
 USE_I18N = True
-
-USE_L10N = True
-
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
