171d3d4f4950a4256a1533b6804ff03d608adf68
影凌 <k6.developer@gmail.com>
2024-07-18 15:12:52 +0800

    fix: wrong folders space

--- RyoURL/RyoURL/urls.py
+++ RyoURL/RyoURL/urls.py
@@ -14,9 +14,18 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 
-urlpatterns = [
-    path('admin/', admin.site.urls),
-]
+from shortURL.api import api
+from shortURL.views import index
+
+urlpatterns = \
+    [
+        path('admin/', admin.site.urls),
+        path('shortURL/', include('shortURL.urls')),
+        path('api/', api.urls),
+        path('', index)
+    ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
