ef09674a663745d80a61a532963d6c268a45d755
影凌 <k6.developer@gmail.com>
2024-07-22 15:24:30 +0800

    test: update test_api.py

--- RyoURL/tests_shortURL/test_api.py
+++ RyoURL/tests_shortURL/test_api.py
@@ -1,47 +1,56 @@
 import pytest
-from django.urls import reverse
 from shortURL.models import Url
+from django.utils import timezone
 
 # 新增短網址 API 測試
 # API：creatShortUrl [POST]
 @pytest.mark.django_db
 def test_creatShortUrl(client):
     response = client.post(
-        reverse('creatShortUrl'), 
-        {'oriUrl': 'https://www.google.com'}
+        '/api/creatShortUrl?oriUrl=https://www.google.com'
     )
-    assert response.status_code == 201 # 201 Created
-    assert 'srtUrl' in response.json() # 檢查 srtUrl 是否有在回傳的 JSON 中（檢查是否成功創立短網址）
+    assert response.status_code == 200  # 200 OK
+    response_data = response.json()
+    assert 'srtUrl' in response_data
+    assert 'oriUrl' in response_data
+    assert 'creDate' in response_data
+    assert response_data['oriUrl'] == "https://www.google.com"
 
 # 以縮短網址查詢原網址 API 測試
 # API：lookforOriUrl [GET]
 @pytest.mark.django_db
 def test_lookforOriUrl(client):
-    url = Url.objects.create(           # 假設一筆短網址資料
+    url = Url.objects.create(
         oriUrl='https://www.example.com', 
         srtUrl='test01',
-        creDate='2000-01-01 00:00:00'
+        creDate=timezone.now()
     )
-    response = client.get(
-        reverse('lookforOriUrl', kwargs={'srtUrl': 'test01'}))
-    assert response.status_code == 200 # 200 OK
-    assert response.json()['oriUrl'] == url.oriUrl # 檢查回傳的原網址是否正確
+    response = client.get(f'/api/lookforOriUrl/{url.srtUrl}')
+    assert response.status_code == 200  # 200 OK
+    response_data = response.json()
+    assert response_data['oriUrl'] == url.oriUrl
+    assert response_data['srtUrl'] == url.srtUrl
+    assert 'creDate' in response_data
 
 # 查詢所有短網址 API 測試
 # API：getAllUrl [GET]
 @pytest.mark.django_db
 def test_getAllUrl(client):
-    # 假設有兩筆短網址資料
     Url.objects.create(
         oriUrl='https://www.example.com', 
         srtUrl='test01',
-        creDate='2000-01-01 00:00:00'
+        creDate=timezone.now()
     )
     Url.objects.create(
-        oriUrl='https://www.example.com', 
+        oriUrl='https://www.example.org', 
         srtUrl='test02',
-        creDate='2000-01-01 00:01:00'
+        creDate=timezone.now()
     )
-    response = client.get(reverse('getAllUrl'))
-    assert response.status_code == 200 # 200 OK
-    assert len(response.json()) == 2   # 檢查回傳的短網址數量是否正確
+    response = client.get('/api/getAllUrl')
+    assert response.status_code == 200  # 200 OK
+    response_data = response.json()
+    assert len(response_data) == 2
+    for url in response_data:
+        assert 'oriUrl' in url
+        assert 'srtUrl' in url
+        assert 'creDate' in url
