fad5fed452e52bffd25696fe8bb65a4929d3c363
影凌 <k6.developer@gmail.com>
2024-07-22 14:52:19 +0800

    style: Add comment

--- RyoURL/shortURL/views.py
+++ RyoURL/shortURL/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import get_object_or_404, redirect
 from .models import Url
 
+# 將短網址導向原網址的函式
 def redirectShortUrl(request, strUrl):
     url = get_object_or_404(Url, srtUrl=strUrl)
     return redirect(url.oriUrl)
