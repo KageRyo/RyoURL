37fb0089c2572b3c7b79f1c2e6f2201422a3acfb
影凌 <k6.developer@gmail.com>
2024-07-29 11:10:01 +0800

    refactor: #15 add ErrorSchema

--- RyoURL/shortURL/api.py
+++ RyoURL/shortURL/api.py
@@ -4,8 +4,8 @@ import datetime
 import requests
 
 from typing import List
-from django.http import HttpResponse
 from ninja import NinjaAPI, Schema
+from ninja.responses import Response
 
 from .models import Url
 
@@ -17,7 +17,11 @@ class UrlSchema(Schema):
     short_string: str
     short_url: str
     create_date: datetime.datetime
-    
+
+# 定義錯誤響應的 Schema
+class ErrorSchema(Schema):
+    message: str
+
 # 產生隨機短網址的函式
 def generator_short_url(length = 6):
     char = string.ascii_letters + string.digits
@@ -49,67 +53,63 @@ def check_url_available(orign_url):
 @api.get("/")
 def index(request):
     return "已與 RyoURL 建立連線。"
-    
+
 # POST : 新增短網址 API /short_url
-@api.post("short_url", response=UrlSchema)
+@api.post("short_url", response={200: UrlSchema, 404: ErrorSchema})
 def create_short_url(request, orign_url: str):
-    orign_url = check_http_format(orign_url)            # 檢查 http 前綴
-    short_string = generator_short_url()                # 產生隨機短網址字符串
-    short_url = handle_domain(request, short_string)    # 處理短網址域名
-    # 如果 URL 無效，則回傳 404
+    orign_url = check_http_format(orign_url)
+    short_string = generator_short_url()
+    short_url = handle_domain(request, short_string)
     if not check_url_available(orign_url):
-        return HttpResponse('URL 不存在或無法存取，請檢查是否出錯。', status=404)
+        return 404, {"message": "URL 不存在或無法存取，請檢查是否出錯。"}
     else:
-        # 建立新的短網址並儲存進資料庫
         url = Url.objects.create(
             orign_url = orign_url,
             short_string = short_string,
             short_url = short_url,
             create_date = datetime.datetime.now()
         )
-        return url
+        return 200, url
 
 # POST : 新增自訂短網址 API /custom_url
-@api.post("custom_url", response=UrlSchema)
+@api.post("custom_url", response={200: UrlSchema, 404: ErrorSchema, 403: ErrorSchema})
 def create_custom_url(request, orign_url: str, short_string: str):
-    orign_url = check_http_format(orign_url)            # 檢查 http 前綴
-    short_url = handle_domain(request, short_string)    # 處理短網址域名
-    # 如果 URL 無效，則回傳 404
+    orign_url = check_http_format(orign_url)
+    short_url = handle_domain(request, short_string)
     if not check_url_available(orign_url):
-        return HttpResponse('URL 不存在或無法存取，請檢查是否出錯。', status=404)
+        return 404, {"message": "URL 不存在或無法存取，請檢查是否出錯。"}
     elif Url.objects.filter(short_url=short_url).exists():
-        return HttpResponse('自訂短網址已存在，請更換其他短網址。', status=403)
+        return 403, {"message": "自訂短網址已存在，請更換其他短網址。"}
     else:
-        # 建立新的短網址並儲存進資料庫
         url = Url.objects.create(
             orign_url = orign_url,
             short_string = short_string,
             short_url = short_url,
             create_date = datetime.datetime.now()
         )
-        return url
+        return 200, url
 
 # GET : 以縮短網址字符查詢原網址 API /orign_url/{short_string}
-@api.get('orign_url/{short_string}', response=UrlSchema)
+@api.get('orign_url/{short_string}', response={200: UrlSchema, 404: ErrorSchema})
 def get_short_url(request, short_string: str):
     try:
         url = Url.objects.get(short_string=short_string)
+        return 200, url
     except Url.DoesNotExist:
-        return HttpResponse('URL not found', status=404)
-    return url
+        return 404, {"message": "URL not found"}
 
 # GET : 查詢所有短網址 API /all_url
 @api.get('all_url', response=List[UrlSchema])
 def get_all_url(request):
     url = Url.objects.all()
     return url
-
+ 
 # DELETE : 刪除短網址 API /short_url/{short_string}
-@api.delete('short_url/{short_string}')
+@api.delete('short_url/{short_string}', response={200: ErrorSchema, 404: ErrorSchema})
 def delete_short_url(request, short_string: str):
     try:
         url = Url.objects.get(short_string=short_string)
+        url.delete()
+        return 200, {"message": "成功刪除！"}
     except Url.DoesNotExist:
-        return HttpResponse('這個短網址並不存在。', status=404)
-    url.delete()
-    return HttpResponse('成功刪除！', status=200)
+        return 404, {"message": "這個短網址並不存在。"}
