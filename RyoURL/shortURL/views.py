1d2479a017774fe29a9937c5a41365c81ad0c041
影凌 <k6.developer@gmail.com>
2024-07-31 16:23:30 +0800

    refactor: make views.py more module

--- RyoURL/shortURL/views.py
+++ RyoURL/shortURL/views.py
@@ -8,15 +8,14 @@ from .models import Url
 # logging 的設定
 logger = logging.getLogger(__name__)
 
-# 將短網址導向原網址的函式
-def redirectShortUrl(request, short_string):
-    url = get_object_or_404(Url, short_string=short_string)
-    
-    # 檢查短網址是否已過期
+# 檢查短網址是否過期的函式
+def is_url_expired(url):
     if url.expire_date and url.expire_date < timezone.now():
         url.delete()
         return HttpResponse("此短網址已過期並已被刪除。", status=404)
-    
+
+# 處理訪問次數與快取的函式
+def handle_visit_count(url):
     # 處理訪問次數
     cache_key = f'visit_count_{url.id}'     # 設定快取的鍵
     visit_count = cache.get(cache_key)      # 從快取中取得訪問次數
@@ -29,11 +28,17 @@ def redirectShortUrl(request, short_string):
     cache.set(cache_key, visit_count, timeout=60*60*24) # 每天重置一次
     logger.debug(f'目前快取中的訪問次數: {visit_count}')
     
-    # 每 10 次訪問更新數據庫
+    # 每 10 次訪問更新數資料庫
     if visit_count % 10 == 0:
         url.visit_count = visit_count
         url.save()
         logger.debug(f'訪問次數儲存進資料庫: {url.visit_count}')
+
+# 將短網址導向原網址的函式
+def redirectShortUrl(request, short_string):
+    url = get_object_or_404(Url, short_string=short_string)
+    is_url_expired(url)  # 檢查短網址是否過期
+    handle_visit_count(url) # 處理訪問次數
     
     # 將使用者重新導向至原網址
     return redirect(url.orign_url)
